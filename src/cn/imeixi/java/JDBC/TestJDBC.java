1ffc34692b2a9fd5b52f625c81d18de7245100f9
imeixi <zheng.ah.r@gmail.com>
2016-03-24 02:20:25 +0800

    first update

---
+++ src/cn/imeixi/java/JDBC/TestJDBC.java
@@ -0,0 +1,18 @@
+package cn.imeixi.java.JDBC;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestJDBC {
+	
+	public static void main(String ...strings){
+		System.out.println("sd");
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
