d0b3b62ee7f9940fa39a92fbde4e1b83dc3e214b
imeixi <zheng.ah.r@gmail.com>
2016-03-24 11:41:59 +0800

    添加oracle链接

--- src/cn/imeixi/java/JDBC/TestJDBC.java
+++ src/cn/imeixi/java/JDBC/TestJDBC.java
@@ -9,11 +9,8 @@ import java.util.Properties;
 
 import org.junit.Test;
 
+
 public class TestJDBC {
-	
-	
-	
-	
 
 	@Test
 	public void testDriver() throws Exception {
@@ -28,9 +25,9 @@ public class TestJDBC {
 //		String user = "root";
 //		String password = "123456";
 		
-		Driver driver = new Driver();
-		String url = "jdbc:mysql://172.31.30.83/test";
-		String user = "root";
+		Driver driver = new oracle.jdbc.driver.OracleDriver();
+		String url = "jdbc:oracle:thin:@172.31.30.84:1521:orcl";
+		String user = "scott";
 		String password = "123456";
 	
 		Properties info = new Properties();
