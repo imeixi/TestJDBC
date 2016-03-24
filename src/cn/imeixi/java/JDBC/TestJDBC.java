e13f313f79346339dfafa8860653be29e2042e5a
imeixi <zheng.ah.r@gmail.com>
2016-03-24 10:38:13 +0800

    mac TestDriver mysql

--- src/cn/imeixi/java/JDBC/TestJDBC.java
+++ src/cn/imeixi/java/JDBC/TestJDBC.java
@@ -2,17 +2,44 @@ package cn.imeixi.java.JDBC;
 
 import static org.junit.Assert.*;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.SQLException;
+import java.util.Properties;
+
 import org.junit.Test;
 
 public class TestJDBC {
 	
-	public static void main(String ...strings){
-		System.out.println("sd");
-	}
+	
+	
+	
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDriver() throws Exception {
+		/**
+		 * 1、创建Driver对象
+		 * 2、定义 String，info
+		 * 3、创建连接Connection对象，使用Driver 对象的connection（url，info）方法。
+		 */
+		
+//		Driver driver = new com.mysql.jdbc.Driver();
+//		String url = "jdbc:mysql://172.31.30.83/test";
+//		String user = "root";
+//		String password = "123456";
+		
+		Driver driver = new Driver();
+		String url = "jdbc:mysql://172.31.30.83/test";
+		String user = "root";
+		String password = "123456";
+	
+		Properties info = new Properties();
+		info.put("user", user);
+		info.put("password", password);
+		
+		Connection connection = driver.connect(url, info);
+		System.out.println(connection);
+	
 	}
 
 }
